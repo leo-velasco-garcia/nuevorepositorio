0038ef91a5d65e5e44fb6c07f6af1774f8377cc4
Daniel Aznar <daniel.aznar2@esdmadrid.es>
2025-05-31 12:39:51 +0200

    imgparrafo

---
+++ components/Body/Imagen.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const props = defineProps({
+        text : String,
+        alt : String,
+    })
+</script>
+<template>
+    <img :src="text" :alt="alt">
+</template>
+<style scoped>
+    img{
+        max-height: calc(100vh - 5rem);
+        width: 100%;
+        border: 1px solid #333333;
+        margin: 1rem 0 1.5rem 0;
+    }
+</style>
---
+++ components/Body/Parrafo.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const props = defineProps({
+        text : String
+    })
+</script>
+<template>
+    <p>{{ text }}</p>
+</template>
+<style scoped>
+    p{
+        font-size: 1rem;
+        line-height: 1.25rem;
+        margin: 0 0 0.5rem 0;
+    }
+</style>
+
